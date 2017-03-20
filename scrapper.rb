e9f8d6dd54e95838943dc79864edcbc9e881a43c
Zac Smith <zrs3141592@gmail.com>
2017-03-20 16:40:13 -0600

    add city and terms parameters

--- scrapper.rb
+++ scrapper.rb
@@ -1,14 +1,14 @@
 require 'open-uri'
 require 'nokogiri'
 
-cities        = ['newyork']
-search_terms  = ['developer']
+cities        = ['denver', 'albuquerque']
+search_terms  = ['ruby']
 
 cities.each do |city|
-  search_terms.each do |terms|
+  search_terms.each do |search_term|
 
     # url = "https://albuquerque.craigslist.org/search/sof"
-    url = "https://newyork.craigslist.org/search/cpg?query=developer&is_paid=all"
+    url = "https://#{city}.craigslist.org/search/cpg?query=#{search_term}&is_paid=all"
 
     document = open(url)
 
@@ -16,9 +16,13 @@ cities.each do |city|
 
     parsed_content = Nokogiri::HTML(content)
 
+    puts "=========================================="
+    puts " #{city} - #{search_term}"
+    puts "=========================================="
+
     # puts parsed_content.css('.content').css('.rows').css('.hdrlnk').first.inner_text
 
-    parsed_content.css('.content').css('.rows').each do |row|
+    parsed_content.css('.content').css('.result-row').each do |row|
       title       = row.css('.hdrlnk').inner_text
       link        = row.css('.hdrlnk').first.attributes["href"].value
       posted_at   = row.css('time').first.attributes["datetime"].value
