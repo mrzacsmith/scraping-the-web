663e011c8ec29c3900e369f1a8d5e97cad68d0de
Zac Smith <zrs3141592@gmail.com>
2017-03-20 16:31:32 -0600

    city and search term function

--- scrapper.rb
+++ scrapper.rb
@@ -4,42 +4,40 @@ require 'nokogiri'
 cities        = ['newyork']
 search_terms  = ['developer']
 
-# cities.each do |city|
-#   search_terms do |terms|
-#
-#
-#   end
-# end
+cities.each do |city|
+  search_terms.each do |terms|
 
+    # url = "https://albuquerque.craigslist.org/search/sof"
+    url = "https://newyork.craigslist.org/search/cpg?query=developer&is_paid=all"
 
-# url = "https://albuquerque.craigslist.org/search/sof"
-url = "https://newyork.craigslist.org/search/cpg?query=developer&is_paid=all"
+    document = open(url)
 
-document = open(url)
+    content = document.read
 
-content = document.read
+    parsed_content = Nokogiri::HTML(content)
 
-parsed_content = Nokogiri::HTML(content)
+    # puts parsed_content.css('.content').css('.rows').css('.hdrlnk').first.inner_text
 
-# puts parsed_content.css('.content').css('.rows').css('.hdrlnk').first.inner_text
+    parsed_content.css('.content').css('.rows').each do |row|
+      title       = row.css('.hdrlnk').inner_text
+      link        = row.css('.hdrlnk').first.attributes["href"].value
+      posted_at   = row.css('time').first.attributes["datetime"].value
+      neighe_elem = row.css('.pnr').css('small')
 
-parsed_content.css('.content').css('.rows').each do |row|
-  title       = row.css('.hdrlnk').inner_text
-  link        = row.css('.hdrlnk').first.attributes["href"].value
-  posted_at   = row.css('time').first.attributes["datetime"].value
-  neighe_elem = row.css('.pnr').css('small')
+      if neighe_elem.any?
+        neighborhood = neighe_elem.first.inner_text.strip
+      else
+        neighborhood = ''
+      end
 
-  if neighe_elem.any?
-    neighborhood = neighe_elem.first.inner_text.strip
-  else
-    neighborhood = ''
+      puts "#{title} #{neighborhood}"
+      puts "Link: #{link}"
+      puts "Posted at #{posted_at}"
+      puts '-------------------------------------'
+    end
   end
-
-  puts "#{title} #{neighborhood}"
-  puts "Link: #{link}"
-  puts "Posted at #{posted_at}"
-  puts '-------------------------------------'
 end
+
 
 # parsed_content.css('.rows').each do |row|
 #   title = row.css('.result-title').inner_text
