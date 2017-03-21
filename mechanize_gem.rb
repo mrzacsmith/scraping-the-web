59ae0660033b5a0221c92c43285f5c4bb7c9fcaa
Zac Smith <zrs3141592@gmail.com>
2017-03-21 17:25:46 -0600

    add block for search terms

--- mechanize_gem.rb
+++ mechanize_gem.rb
@@ -1,10 +1,26 @@
 require 'mechanize'
+
+# search terms
+search_terms = ['steve jobs', 'henry ford', 'andrew carnegie', 'benjamin franklin']
+
+# set up mechanize
 agent = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
 }
 
+# set up page
 page = agent.get('https://www.wikipedia.org/')
-
 search_form = page.form(:action => "//www.wikipedia.org/search-redirect.php")
 
-pp search_form
+search_terms.each do |term|
+  search_form.search = term
+  results = agent.submit(search_form, search_form.button('go'))
+  html_results = Nokogiri::HTML results.body
+  puts html_results.at_css('#firstHeading').text
+
+end
+
+# search_form.search = 'Steve Jobs'
+# results = agent.submit(search_form, search_form.button('go'))
+# html_results = Nokogiri::HTML results.body
+# puts html_results.at_css('#firstHeading').text
