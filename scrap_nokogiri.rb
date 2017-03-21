913abd0629cb1384657409c1662e6cc43d2d56bc
Zac Smith <zrs3141592@gmail.com>
2017-03-21 12:24:26 -0600

    inital scrap_nokogiri

--- scrap_nokogiri.rb
+++ scrap_nokogiri.rb
@@ -3,5 +3,15 @@ require 'nokogiri'
 file = File.open('steve_jobs.htm')
 html = Nokogiri::HTML file
 
-name = html.at_css('body').text.strip
-# html.css('body').text.strip
+output = File.new('steve-story.txt', 'w+')
+
+html.at_css('body').text.strip
+name = html.at_css('#firstHeading').text
+bday = html.at_css('.bday').text
+dday = html.at_css('.dday').text
+cause_of_death = html.at_xpath('//a[@title="Pancreatic cancer"]').text
+
+# output.write(name + ' was born ' + bday + ".\n" )
+# output.write("He died on " + dday + " from " + cause_of_death)
+
+output.write("\n#{name} was born #{bday} and died on #{dday} from #{cause_of_death}")
