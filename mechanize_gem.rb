e7e8eb264487b0802e59e563f969f8db074a97ec
Zac Smith <zrs3141592@gmail.com>
2017-03-22 10:06:23 -0600

    complete mechanize program

--- mechanize_gem.rb
+++ mechanize_gem.rb
@@ -1,4 +1,5 @@
 require 'mechanize'
+require 'CSV'
 
 # search terms
 search_terms = ['steve jobs', 'henry ford', 'andrew carnegie', 'benjamin franklin']
@@ -16,8 +17,17 @@ search_terms.each do |term|
   search_form.search = term
   results = agent.submit(search_form, search_form.button('go'))
   html_results = Nokogiri::HTML results.body
-  puts html_results.at_css('#firstHeading').text
+  name = html_results.at_css('#firstHeading').text
+  bday = html_results.at_css('.bday').text
+  dday = html_results.at_css('.dday').text
 
+  # puts "#{name} was born on #{bday} and died on #{dday}."
+  CSV.open('life-data.csv', 'a+') do |csv|
+    csv << [name, bday, dday]
+  end
+  delay_time = rand(61)
+  sleep(delay_time)
+  puts "#{name} was just added to CSV!"
 end
 
 # search_form.search = 'Steve Jobs'
